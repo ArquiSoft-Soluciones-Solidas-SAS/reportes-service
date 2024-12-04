48630ed1ef5a1097638f182fa3d944d0c82ef9a0
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-12-04 13:07:44 -0500

    cxc

--- ofipensiones/reportesService/services.py
+++ ofipensiones/reportesService/services.py
@@ -11,7 +11,7 @@ from mongoengine.queryset.visitor import Q
 def obtener_cuentas_por_cobrar(nombre_institucion, mes):
     print("Hit the DB")
 
-    # Filtrar la institución y el mes
+    # Filtrar los cronogramas base de la institución
     cronogramas = CronogramaBase.objects.filter(
         nombreInstitucion=nombre_institucion
     ).only("id", "detalle_cobro", "nombre")
@@ -19,20 +19,27 @@ def obtener_cuentas_por_cobrar(nombre_institucion, mes):
     processed_rows = []
 
     for cronograma in cronogramas:
-        # Filtrar detalles del cronograma para el mes específico
+        # Filtrar los detalles del cronograma para el mes especificado
         detalles_mes = [detalle for detalle in cronograma.detalle_cobro if detalle.mes == mes]
         for detalle_cobro in detalles_mes:
             # Buscar los recibos de cobro relacionados con este detalle
             recibos = ReciboCobro.objects.filter(
                 detalles_cobro__id=detalle_cobro.id,
-                estudiante__nombreInstitucion=nombre_institucion,
                 id__nin=ReciboPago.objects.distinct("recibo_cobro")  # Recibos no pagados
             ).only("nmonto", "detalles_cobro", "estudiante")
 
             for recibo in recibos:
-                estudiante = Estudiante.objects.get(id=recibo.estudiante)
+                # Obtener el objeto Estudiante relacionado
+                estudiante = Estudiante.objects.get(id=recibo.estudiante.id)
+
+                # Filtrar por institución del estudiante
+                if estudiante.nombreInstitucion != nombre_institucion:
+                    continue
+
+                # Obtener el curso del estudiante
                 curso = Curso.objects.get(id=estudiante.cursoEstudianteId)
 
+                # Procesar los datos para la salida
                 processed_rows.append({
                     "monto_recibo": float(recibo.nmonto),
                     "mes": detalle_cobro.mes,
