8c5b5a1904af8528d1957a414b389ed7243a5b78
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-12-04 20:19:12 -0500

    urls

--- ofipensiones/ofipensiones/urls.py
+++ ofipensiones/ofipensiones/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reportes/', include('reportesService.urls')),
+    path(r'', include('django.contrib.auth.urls')),
+    path(r'', include('social_django.urls')),
 ]
