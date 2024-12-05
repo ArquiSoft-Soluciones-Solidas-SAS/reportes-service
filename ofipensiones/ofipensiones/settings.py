f14c9ca06324fb9208968b24797d4de3d384a0ed
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-12-04 20:16:20 -0500

    intento auth0

--- ofipensiones/ofipensiones/settings.py
+++ ofipensiones/ofipensiones/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reportesService',
-
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -145,4 +145,25 @@ CQRS = {
     'user': 'microservicios_user',
     'password': 'password',
     'rabbit-queue': 'cqrs_replica',  # settings used only in replica
+}
+
+LOGIN_URL = "/login/auth0"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "https://dev-73advmhx411p43vz.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F35.196.182.237:8080"
+
+SOCIAL_AUTH_TRAILING_SLASH = False  # Remove end slash from routes
+SOCIAL_AUTH_AUTH0_DOMAIN = 'dev-73advmhx411p43vz.us.auth0.com'
+SOCIAL_AUTH_AUTH0_KEY = 'U9yCcgGMaZWzMz92AY9PVSDvEcaoxbtl'
+SOCIAL_AUTH_AUTH0_SECRET = 'eDF2-rr5GXWk3m_gOacgdUA2-5mRUWAMeLMrSDStriRd7-5NEUsz7PP8CRp-d6wc'
+
+SOCIAL_AUTH_AUTH0_SCOPE = [
+    'openid',
+    'profile',
+    'email',
+    'role',
+]
+
+AUTHENTICATION_BACKENDS = {
+    'AppDjango.auth0backend.Auth0',
+    'django.contrib.auth.backends.ModelBackend',
 }
