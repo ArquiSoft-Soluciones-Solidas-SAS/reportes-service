d5240f365ba7313b8c6cc1306eb990d1ee023d36
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-12-05 11:19:31 -0500

    reporte

--- ofipensiones/reportesService/urls.py
+++ ofipensiones/reportesService/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import generar_reporte#,generar_cartera
+from .views import generar_reporte, listar_instituciones  # ,generar_cartera
 
 app_name = 'manejador_reportes'  # Esto es necesario para el namespace
 urlpatterns = [
     path('', generar_reporte, name='generar_reporte'),
     path('reporte/<str:nombre_institucion>/<str:mes>/', generar_reporte, name='generar_reporte'),
-    #path('cartera_general/<str:nombre_institucion>/<str:mes>/', generar_cartera, name='generar_cartera'),
+    path('reporte/instituciones/', listar_instituciones, name='listar_instituciones'),
 ]
