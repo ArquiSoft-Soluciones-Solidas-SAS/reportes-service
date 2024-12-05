8f412fec3ecff7e4c7beccf3b5075b89b0fb4139
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-12-04 21:18:38 -0500

    db

--- ofipensiones/ofipensiones/settings.py
+++ ofipensiones/ofipensiones/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'ofipensiones.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 MONGODB_DATABASES = {
     "default": {
